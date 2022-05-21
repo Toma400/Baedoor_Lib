0bcff68cfef3ec5295aa936c04bee4fc73f2adb5
Toma400 <thomasso_calgori@o2.pl>
2022-05-21 10:53:43 +0200

    Innitial setup

---
+++ src/main/java/toma400/baedoor_lib/BaedoorLib.java
@@ -0,0 +1,31 @@
+package toma400.baedoor_lib;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+@Mod(BaedoorLib.MOD_ID)
+public class BaedoorLib
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "baedoor_lib";
+
+    public BaedoorLib() {
+
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        eventBus.addListener(this::setup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+
+    }
+}
