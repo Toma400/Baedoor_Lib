e621e9e345d316e81850d1d013406a84f284fc38
Toma400 <thomasso_calgori@o2.pl>
2022-05-26 16:52:09 +0200

    Small stuff

--- src/main/java/toma400/baedoor_lib/BaedoorLib.java
+++ src/main/java/toma400/baedoor_lib/BaedoorLib.java
@@ -1,22 +1,12 @@
 package toma400.baedoor_lib;
 
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.stream.Collectors;
 
 @Mod(BaedoorLib.MOD_ID)
 public class BaedoorLib
